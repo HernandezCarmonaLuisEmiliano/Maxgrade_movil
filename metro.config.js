cb521495e0b2113ee56c4edc25fa5775ae52809d
Salinas-Luis <lsalinasr2300@alumno.ipn.mx>
2026-05-28 18:57:16 -0600

    ultimos cambios nose xd

--- metro.config.js
+++ metro.config.js
@@ -2,8 +2,18 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+config.transformer = {
+  ...config.transformer,
+  getTransformOptions: async () => ({
+    transform: {
+      experimentalImportSupport: false,
+      inlineRequires: true,
+    },
+  }),
+};
+
 config.resolver.resolveRequest = (context, moduleName, platform) => {
-  if (moduleName.includes('@opentelemetry')) {
+  if (moduleName === '@opentelemetry/api' || moduleName.includes('@opentelemetry')) {
     return { type: 'empty' };
   }
   return context.resolveRequest(context, moduleName, platform);
